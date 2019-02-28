9a207b982013a25b8f86b81a3f85a00bd1e4c383
taurus-git <t.1113@ya.ru>
2019-02-28 15:49:31 +0200

    Used Repeater from ACF.

--- wp-content/themes/wplanding/landing-parts/timeline.php
+++ wp-content/themes/wplanding/landing-parts/timeline.php
@@ -1,7 +1,6 @@
 <?php
 $args = array(
     'post_type' => 'timeline',
-    'order' => 'ASC'
 );
 
 $query = new WP_Query($args);
@@ -9,17 +8,19 @@ $query = new WP_Query($args);
 if ($query->have_posts()) {
     while ($query->have_posts()) {
         $query->the_post();
-        ?>
-        <li>
-            <div class="box-time">
-                <span><?php the_field('time'); ?></span>
-            </div>
-            <div class="box-txt">
-                <strong><?php the_field('name'); ?></strong>
-                <p><?php the_field('description'); ?></p>
-            </div>
-        </li>
+
+        while ( have_rows('timeline') ) : the_row(); ?>
+            <li>
+                <div class="box-time">
+                    <span><?php the_sub_field('time'); ?></span>
+                </div>
+                <div class="box-txt">
+                    <strong><?php the_sub_field('name'); ?></strong>
+                    <p><?php the_sub_field('description'); ?></p>
+                </div>
+            </li>
         <?php
+        endwhile;
     }
 
 }
